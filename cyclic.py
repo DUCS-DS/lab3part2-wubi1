c5d5ae08161bdda8829ef49760b69cf8f7b60d8c
wubihalefom <whalefom@drury.com>
2023-03-06 20:58:17 -0600

    lab complete

--- cyclic.py
+++ cyclic.py
@@ -1,14 +1,48 @@
+#Wubi Halefom
+#03/6/23
+
 from llist import *
+from gencyclic import *
+
+
+def getNext(Node):
+    return Node.next
 
 def llprint(lst, num):
-    """print the first num terms of linked list lst"""
-    pass
+    count = 0
+    currentNode = lst.head
+    for i in range(num):
+        print(currentNode.val, end=' ')
+        currentNode = currentNode.next
+        
 
 def find_cycle(lst):
-    """return the start index and length of the cycle"""
-    pass
+ 
+    tortoise = getNext(lst.head)
+    hare = getNext(getNext(lst.head))
+    while tortoise != hare:
+        tortoise = getNext(tortoise)
+        hare = getNext(getNext(hare))
+  
+    mu = 0
+    tortoise = lst.head
+    while tortoise != hare:
+        tortoise = getNext(tortoise)
+        hare = getNext(hare)   
+        mu += 1
+ 
+    lam = 1
+    hare = getNext(tortoise)
+    while tortoise != hare:
+        hare = getNext(hare)
+        lam += 1
+ 
+    return lam, mu
 
 
 if __name__ == "__main__":
+    tup = find_cycle(lst)
+    print(f"cycle start: {tup[0]}")
+    print(f"length: {tup[1]}")
 
-    pass
+    llprint(cyclic(10),5)
